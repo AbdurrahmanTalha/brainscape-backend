d2c391133a4aade85d2d1061feb168a8cd05021b
AbdurrahmanTalha <abdurrahmantalha.dev@gmail.com>
2023-09-11 00:02:45 +0600

    [DONE] Cors update

--- src/app.ts
+++ src/app.ts
@@ -8,13 +8,31 @@ import globalErrorHandler from "./app/middlewares/globalErrorHandler";
 
 const app: Application = express();
 
-app.use(cors());
+const corsOptions = {
+    origin: [
+        "http://localhost:3000",
+        "https://brainscape-backend.vercel.app",
+        "https://brainscape-frontend.vercel.app",
+    ], // Add your allowed origins here
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true, // Enable cookies and authorization headers
+};
+
+app.use(cors(corsOptions));
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/v1", routes);
 app.use(globalErrorHandler);
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+});
 
 app.use((req: Request, res: Response, next: NextFunction) => {
     res.status(httpStatus.NOT_FOUND).json({
