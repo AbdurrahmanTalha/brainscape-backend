e41b7f2e369cff26d6c1394327fdc33b98a543a4
Abdur Rahman Talha <66727914+AbdurrahmanTalha@users.noreply.github.com>
2024-02-14 10:59:24 +0600

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 /* eslint-disable no-console */
 import { Server } from "http";
 import mongoose from "mongoose";
-import app from "./app";
+import app from "./app"; // asdasdasd asdnausdog4ift43ift43t
 import config from "./config/index";
 // testttttasdasdaasdas asdasdas dasda/sdasdansdasasdasdasdudnasud
 process.on("uncaughtException", error => {
